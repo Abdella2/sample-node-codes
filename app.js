6873e03e762bdaf018a16f82894b19ee7a74eede
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-23 15:25:59 +0300

    Initial commit.

---
+++ app.js
@@ -0,0 +1,8 @@
+// /*jshint esversion: 11 */
+global.console.log('well come');
+var message = 'wow';
+console.log(global.message);
+console.log(module);
+const logger = require('./logger');
+
+logger.log('message');
---
+++ logger.js
@@ -0,0 +1,9 @@
+console.log(__filename);
+console.log(__dirname);
+const url = '...';
+
+function log(message) {
+  console.log(message);
+}
+
+module.exports.log = log;

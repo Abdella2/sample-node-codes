28ec2eaf7fc8892ac73ff6dc056a9fd9b71abebf
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-25 12:34:25 +0300

    test process object.

---
+++ test_process_object.js
@@ -0,0 +1,5 @@
+console.log('================= process object ===========');
+console.log(process);
+console.log('================= process properties ===========');
+console.log(`process id: ${process.pid}`);
+console.log(`process id: ${process.env.PORT}`);

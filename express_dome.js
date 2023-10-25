c8ea48a5aacf011978ac949422f45995a20e562c
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-25 12:15:06 +0300

    test get request.

--- express_dome.js
+++ express_dome.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.send('hello world'));
+
+app.get('/api/customers', (req, res) =>
+  res.send([{ id: 1, name: 'test customer' }])
+);
+
 app.listen('3000', () => console.log('Server is listening on port 3000...'));

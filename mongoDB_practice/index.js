9ab0175299a996a69abfcd48c3e6ae7bdd4c6d02
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-29 14:56:13 +0300

    Test connection to mongodb.

--- mongoDB_practice/index.js
+++ mongoDB_practice/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const mongoose = require('mongoose');
+const appInfo = require('debug')('app:info');
+const appError = require('debug')('app:error');
+
+mongoose
+  .connect('mongodb://127.0.0.1/mongo_practice')
+  .then(() => appInfo('Connected to mongodb.'))
+  .catch((err) => appError("can't connect to mongodb", err.message));

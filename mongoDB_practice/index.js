ca5e97620c44a379f5877d26e1e32029f3102e77
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-29 15:39:09 +0300

    Move connection string to config file.

--- mongoDB_practice/index.js
+++ mongoDB_practice/index.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const appInfo = require('debug')('app:info');
 const appError = require('debug')('app:error');
+const config = require('config');
+
+const dbUrl = `${config.get('db.host')}:${config.get('db.port')}/${config.get(
+  'db.dbName'
+)}`;
 
 mongoose
-  .connect('mongodb://127.0.0.1/mongo_practice')
+  .connect(dbUrl)
   .then(() => appInfo('Connected to mongodb.'))
   .catch((err) => appError("can't connect to mongodb", err.message));

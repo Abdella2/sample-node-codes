0366797d5b0cf39a3961b534e0909968101c7aa9
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-30 11:57:12 +0300

    Mongoose build in validators.

--- mongoDB_practice/data_validation.js
+++ mongoDB_practice/data_validation.js
@@ -14,18 +14,76 @@ const mongoose = require('./index').mongoose;
 //   .catch((err) => appError("can't connect to mongodb", err.message));
 
 const employeeSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  employeeNo: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 255,
+    match: /^EMP[0-9]{3}/i
+  },
+  name: { type: String, required: true },
+  salary: {
+    type: Number,
+    min: [1000, 'should be greater than 1000'],
+    max: 1200
+  },
+  paymentMethod: {
+    type: String,
+    enum: ['Cash', 'Credit'],
+    required: function () {
+      return this.salary > 1000 && this.salary < 1200;
+    }
+  },
+  phone: {
+    type: String,
+    required: [true, 'Employee phone number required'],
+    validate: {
+      validator: function (v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`
+    }
+  },
+  departments: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: 'Employee should has at least one department.'
+    }
+  }
 });
 
 const Employee = mongoose.model('employee', employeeSchema);
 
-const employee = new Employee({});
+const employee = new Employee({
+  name: 'John',
+  employeeNo: 'EMP012',
+  salary: 1100,
+  paymentMethod: 'Cash',
+  phone: 's',
+  departments: null
+});
 
 async function createEmployee() {
+  // const error = employee.validateSync();
+
+  // if (error) {
+  //   appError('Errors: ', error.errors['name']);
+  //   appError('Errors Message: ', error.errors['name'].message);
+  //   appError('Path: ', error.errors['name'].path);
+  //   appError('Type: ', error.errors['name'].kind);
+  //   appError('Value: ', error.errors['name'].value);
+  //   appError('Message', error.message);
+  //   return;
+  // }
+
   try {
     const employeeInDB = await employee.save();
   } catch (ex) {
-    appError(ex.message);
+    for (field in ex.errors)
+      appError(`${ex.errors[field].path} -> ${ex.errors[field].message}`);
   }
 }
 

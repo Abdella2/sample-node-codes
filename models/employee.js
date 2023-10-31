d576763c7e84732f87f0b05b83cec24e1f7046fd
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-10-31 11:39:30 +0300

    Restructure the project.

---
+++ models/employee.js
@@ -0,0 +1,76 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Employee = mongoose.model(
+  'Employee',
+  new mongoose.Schema({
+    employeeNo: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 255,
+      match: /^EMP[0-9]{3}/i
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String
+    },
+    salary: {
+      type: Number,
+      min: [1000, 'should be greater than 1000'],
+      max: 1200
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['Cash', 'Credit'],
+      required: function () {
+        return this.salary > 1000 && this.salary < 1200;
+      }
+    },
+    phone: {
+      type: String,
+      required: [true, 'Employee phone number required'],
+      validate: {
+        validator: function (v) {
+          return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`
+      }
+    },
+    departments: {
+      type: Array,
+      validate: {
+        validator: function (v) {
+          return v && v.length > 0;
+        },
+        message: 'Employee should has at least one department.'
+      }
+    }
+  })
+);
+
+function validateEmployee(employee) {
+  const schema = Joi.object({
+    name: Joi.string().required().min(5),
+    phone: Joi.string()
+      .required()
+      .pattern(new RegExp(/\d{3}-\d{3}-\d{4}/)),
+    employeeNo: Joi.string()
+      .required()
+      .min(1)
+      .max(255)
+      .pattern(/^EMP[0-9]{3}/i),
+    email: Joi.string(),
+    salary: Joi.number().min(1000).max(1200),
+    paymentMethod: Joi.string(),
+    departments: Joi.array()
+  });
+
+  return schema.validate(employee);
+}
+
+exports.Employee = Employee;
+exports.validate = validateEmployee;
--- routes/employees.js
+++ routes/employees.js
@@ -1,59 +1,7 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
 const express = require('express');
+const { Employee, validate } = require('../models/employee');
 
 const router = express.Router();
-
-const Employee = mongoose.model(
-  'Employee',
-  new mongoose.Schema({
-    employeeNo: {
-      type: String,
-      required: true,
-      minlength: 1,
-      maxlength: 255,
-      match: /^EMP[0-9]{3}/i
-    },
-    name: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String
-    },
-    salary: {
-      type: Number,
-      min: [1000, 'should be greater than 1000'],
-      max: 1200
-    },
-    paymentMethod: {
-      type: String,
-      enum: ['Cash', 'Credit'],
-      required: function () {
-        return this.salary > 1000 && this.salary < 1200;
-      }
-    },
-    phone: {
-      type: String,
-      required: [true, 'Employee phone number required'],
-      validate: {
-        validator: function (v) {
-          return /\d{3}-\d{3}-\d{4}/.test(v);
-        },
-        message: (props) => `${props.value} is not a valid phone number!`
-      }
-    },
-    departments: {
-      type: Array,
-      validate: {
-        validator: function (v) {
-          return v && v.length > 0;
-        },
-        message: 'Employee should has at least one department.'
-      }
-    }
-  })
-);
 
 // let employees = [];
 
@@ -91,7 +39,7 @@ router.post('/', async (req, res) => {
 
   // console.log(valResult);
 
-  const { error } = validateEmployee(req.body);
+  const { error } = validate(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -115,7 +63,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateEmployee(req.body);
+  const { error } = validate(req.body);
 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -155,25 +103,5 @@ router.delete('/:id', async (req, res) => {
 
   res.send(employee);
 });
-
-function validateEmployee(employee) {
-  const schema = Joi.object({
-    name: Joi.string().required().min(5),
-    phone: Joi.string()
-      .required()
-      .pattern(new RegExp(/\d{3}-\d{3}-\d{4}/)),
-    employeeNo: Joi.string()
-      .required()
-      .min(1)
-      .max(255)
-      .pattern(/^EMP[0-9]{3}/i),
-    email: Joi.string(),
-    salary: Joi.number().min(1000).max(1200),
-    paymentMethod: Joi.string(),
-    departments: Joi.array()
-  });
-
-  return schema.validate(employee);
-}
 
 module.exports = router;

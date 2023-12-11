7b66a4dfefbc657bc68f857c63bf32affed52192
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-11 14:19:24 +0300

    Create a user model.

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, minlength: 5, maxlength: 50, required: true },
+  email: {
+    type: String,
+    minlength: 5,
+    maxlength: 255,
+    required: true,
+    unique: true
+  },
+  password: { type: String, required: true, minlength: 5, maxlength: 1024 }
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validate(user) {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).required(255)
+  });
+
+  return schema.validate(user);
+}
+
+// exports.User = User;
+// exports.validate = validate;
+module.exports = {
+  User,
+  validate
+};

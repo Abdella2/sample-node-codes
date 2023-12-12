dbc3fe9322471ba5b8cf76db2c071945e1429c12
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-12 19:27:42 +0300

    Hashing password.

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const { User, validate } = require('../models/user');
 const express = require('express');
@@ -16,6 +17,8 @@ router.post('/', async (req, res) => {
     return res.status(400).send('User with the given email already exist.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   try {
     await user.save();
